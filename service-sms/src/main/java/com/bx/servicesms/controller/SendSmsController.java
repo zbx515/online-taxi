7bed019325e175c4e2d1f9727f59d91a0c1bc459
zbx515 <545117953@qq.com>
2021-04-13 21:07:00 +0800

    111

--- service-sms/src/main/java/com/bx/servicesms/controller/SendSmsController.java
+++ service-sms/src/main/java/com/bx/servicesms/controller/SendSmsController.java
@@ -4,6 +4,7 @@ import com.bx.internalcommon.dto.ResponseResult;
 import com.bx.internalcommon.dto.verifservice.VerifCodeEntity;
 import com.bx.servicesms.service.SendSmsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,19 @@ public class SendSmsController {
     @Autowired
     private SendSmsService sendSmsService;
 
+    @Value("${server.port}")
+    private int port;
+
     @RequestMapping("/send")
     public ResponseResult send(@RequestBody VerifCodeEntity verifCodeEntity){
         String phoneNumber = verifCodeEntity.getPhoneNumber();
         String verifCode = verifCodeEntity.getVerifCode();
         //发送短信的动作，然后返回
         return sendSmsService.send(phoneNumber,verifCode);
+    }
+
+    @RequestMapping("/test")
+    public String test(){
+        return "service-sms: "+port;
     }
 }
