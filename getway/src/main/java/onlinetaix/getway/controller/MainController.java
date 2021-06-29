bdcc63f8c3b98ca7cf9f57fb52e8e12026b4c8b4
zbx515 <545117953@qq.com>
2021-06-29 23:43:01 +0800

    1

---
+++ getway/src/main/java/onlinetaix/getway/controller/MainController.java
@@ -0,0 +1,13 @@
+package onlinetaix.getway.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("getWay")
+    public String getWay(){
+        return null;
+    }
+}
